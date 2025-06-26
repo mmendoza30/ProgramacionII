a387df9e9d60aa497a450bb1f58ef9f881009daf
Mónica Mendoza Morales <moniik3010@gmail.com>
2025-06-25 20:23:45 -0600

    feat:Añadida la funcion que retorne el maximo de 3 valores

--- Practica.py
+++ Practica.py
@@ -6,5 +6,5 @@ df = pd.read_csv("diabetes.csv")
 print("Dimensión de la tabla:", df.shape)
 
 # Función que retorna el menor de tres valores
-def menor(A, B, C):
-    return min(A, B, C)
+def maximo(A, B, C):
+    return max(A, B, C)
